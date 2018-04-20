8f0fb2ad93a3e59b7d0b7997c516206f8df7206c
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-04-21 00:59:58 +0530

    Added Network part for controlling

--- control.js
+++ control.js
@@ -1,11 +1,19 @@
 var SerialPort = require("serialport");
 var socket = require('socket.io-client')('http://34.212.83.92:6001');
 
+// var socket = require('socket.io-client')('http://127.0.0.1:6001');
+
 var spawn = require("child_process").spawn;
 
-spawn('python',["control.py",-10,3,-5]);
+// spawn('python',["control.py",-10,3,-5]);
 
+socket.on('connect',function(){
+	console.log("Local Machine is connected to the server");
+});
 
+socket.on('control',function(data) {
+      console.log('Received Control Message Recieved from the server! ',data);
+});
 // socket.on('newComand', function (data) {
 // 	// Check if a new command has been excecuted
 
