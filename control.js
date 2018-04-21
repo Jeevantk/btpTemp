b58bc2010af7d5ed79a14260dd2f88048050b1a7
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-04-21 11:55:07 +0530

    sd

--- control.js
+++ control.js
@@ -5,7 +5,7 @@ var socket = require('socket.io-client')('http://34.212.83.92:6001');
 
 var spawn = require("child_process").spawn;
 
-// spawn('python',["control.py",-10,3,-5]);
+
 
 socket.on('connect',function(){
 	console.log("Local Machine is connected to the server");
@@ -13,7 +13,11 @@ socket.on('connect',function(){
 
 socket.on('control',function(data) {
       console.log('Received Control Message Recieved from the server! ',data);
+      spawn('python',["control.py",data.xValue,data.yValue,data.zValue,data.feedRate]);
+      // console.log(data.xValue);
 });
+
+
 // socket.on('newComand', function (data) {
 // 	// Check if a new command has been excecuted
 
