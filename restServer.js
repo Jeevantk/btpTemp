37cb1506ec4f0b847e4d4ccc397716abe6c53f5b
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-05-03 09:57:25 +0530

    Added error Handlers

--- restServer.js
+++ restServer.js
@@ -35,23 +35,24 @@ connection.connect((err)=>{
   console.log('Connected to MYSQL Database');
 });
 
-// app.use(function(req, res, next) {
-//     var error404 = new Error('Route Not Found');
-//     error404.status = 404;
-//     next(error404);
-// });
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
 
 
-
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 app.listen(restPort,function(){
   console.log("Rest API endpoint Active at port %d",restPort);
