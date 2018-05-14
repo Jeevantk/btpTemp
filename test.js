fed5fb8bc80ec2ee86ee6c65fe5336459e52e767
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-05-14 09:13:46 +0530

    Changed Ip

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-var socket = require('socket.io-client')('http://34.212.83.92:6001');
+var socket = require('socket.io-client')('http://54.190.43.20:6001');
 
 socket.on('connect',function(){
 	console.log("Local Machine is connected to the server");
 });
-
 
 
 // socket.emit('new Current',{"variance":10,"value":64});
